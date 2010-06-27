6c496800e7f0017c9b7a92d1f281e37f053d8c18
Jim Sun <zimingsun@gmail.com>
2010-06-26 23:44:20 -0700

    add tests for controllers - user (p223)

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -2,7 +2,38 @@ require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "index" do
+    get :index
+    # assert_response :success #200
+    assert_response :redirect #302
+  end
+
+  test "index without user" do
+    get :index
+    assert_redirected_to :action=>"login"
+    assert_equal "Please log in", flash[:notice]
+  end
+
+  fixtures :users
+  test "index with user" do
+    # here the {} parameter is used for passing into the action
+    # the 3rd parameter is used for populating the session data
+    get :index, {}, {:user_id=>users(:dave).id}
+    assert_response :success
+    assert_template "index"
+  end
+
+  test "login" do
+    dave =users(:dave)
+    # notice we are doing a post action here
+    post :login, :name=>dave.name, :password=>'secret'
+    assert_redirected_to :action=>"index"
+    assert_equal dave.id, session[:user_id]
+  end
+
+  test "bad password" do
+    dave =users(:dave)
+    post :login, :name=>dave.name, :password=>'wrong'
+    assert_template "login"
   end
 end
